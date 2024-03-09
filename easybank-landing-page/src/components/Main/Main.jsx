2ae6d8eb67235dbcbc4516e40cdb07e4c647e804
jackie247 <jackieyu247@gmail.com>
2024-03-09 17:27:05 +0000

    remove unused import

--- easybank-landing-page/src/components/Main/Main.jsx
+++ easybank-landing-page/src/components/Main/Main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../Hero/Hero';
 import FeaturesSection from '../FeaturesSection/FeaturesSection';
 import ArticlesSection from '../ArticlesSection/ArticlesSection';
-import Footer from '../Footer/Footer';
 
 export default function Main() {
   return (
